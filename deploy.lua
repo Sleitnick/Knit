64ae4e1af45b3493c03a03e3123151cd2714bca0
Stephen Leitnick <sleitnick@gmail.com>
2020-08-10 00:21:48 -0400

    Autobuild

---
+++ deploy.lua
@@ -0,0 +1,12 @@
+-- Remodel Deploy script
+
+local KNIT_ASSET_ID = "5530714855"
+
+print("Loading Knit")
+local place = remodel.readPlaceFile("Knit.rbxlx")
+local Knit = place.ReplicatedStorage.Knit
+
+print("Writing Knit module to Roblox...")
+remodel.writeExistingModelAsset(Knit, KNIT_ASSET_ID)
+
+print("Knit module written")
