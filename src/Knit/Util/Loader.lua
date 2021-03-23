61e142f05250a6d01174552b23542ca463a0d540
Christopher Chang <51393127+Krysiene@users.noreply.github.com>
2021-03-23 09:12:55 -0700

    Optimize table insertion

--- src/Knit/Util/Loader.lua
+++ src/Knit/Util/Loader.lua
@@ -17,10 +17,12 @@ local Loader = {}
 
 function Loader.LoadChildren(parent)
 	local modules = {}
+	local n = 0
 	for _,child in ipairs(parent:GetChildren()) do
 		if (child:IsA("ModuleScript")) then
 			local m = require(child)
-			table.insert(modules, m)
+			n += 1
+			modules[n] = m
 		end
 	end
 	return modules
@@ -29,10 +31,12 @@ end
 
 function Loader.LoadDescendants(parent)
 	local modules = {}
+	local n = 0
 	for _,descendant in ipairs(parent:GetDescendants()) do
 		if (descendant:IsA("ModuleScript")) then
 			local m = require(descendant)
-			table.insert(modules, m)
+			n += 1
+			modules[n] = m
 		end
 	end
 	return modules
