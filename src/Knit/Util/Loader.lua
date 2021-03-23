7c285bcb01c65a8c1b7d4b3c657565277d9dd6a2
Christopher Chang <51393127+Krysiene@users.noreply.github.com>
2021-03-23 10:44:53 -0700

    Update Loader.lua

--- src/Knit/Util/Loader.lua
+++ src/Knit/Util/Loader.lua
@@ -3,12 +3,9 @@
 -- January 10, 2021
 
 --[[
-
 	Loads all ModuleScripts within the given parent.
-
 	Loader.LoadChildren(parent: Instance): module[]
 	Loader.LoadDescendants(parent: Instance): module[]
-
 --]]
 
 
@@ -17,12 +14,10 @@ local Loader = {}
 
 function Loader.LoadChildren(parent)
 	local modules = {}
-	local n = 0
 	for _,child in ipairs(parent:GetChildren()) do
 		if (child:IsA("ModuleScript")) then
 			local m = require(child)
-			n += 1
-			modules[n] = m
+			table.insert(modules, m)
 		end
 	end
 	return modules
@@ -31,12 +26,10 @@ end
 
 function Loader.LoadDescendants(parent)
 	local modules = {}
-	local n = 0
 	for _,descendant in ipairs(parent:GetDescendants()) do
 		if (descendant:IsA("ModuleScript")) then
 			local m = require(descendant)
-			n += 1
-			modules[n] = m
+			table.insert(modules, m)
 		end
 	end
 	return modules
