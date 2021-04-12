1bec18ccfc6e96f263bb345f81365fd6c4415c83
Stephen Leitnick <sleitnick@gmail.com>
2021-04-12 18:49:30 -0400

    Fix bug for Streamable to properly handle instances that are immediately available

--- src/Knit/Util/Streamable.lua
+++ src/Knit/Util/Streamable.lua
@@ -5,7 +5,7 @@
 --[[
 
 	streamable = Streamable.new(parent: Instance, childName: string)
-	
+
 	streamable:Observe(handler: (child: Instance, maid: Maid) -> void): Connection
 	streamable:Destroy()
 
@@ -22,27 +22,42 @@ Streamable.__index = Streamable
 
 
 function Streamable.new(parent, childName)
+
 	local self = setmetatable({}, Streamable)
+
 	self._maid = Maid.new()
 	self._shown = Signal.new(self._maid)
 	self._shownMaid = Maid.new()
 	self._maid:GiveTask(self._shownMaid)
+
 	self.Instance = parent:FindFirstChild(childName)
-	self._maid:GiveTask(parent.ChildAdded:Connect(function(child)
+
+	local function OnInstanceSet()
+		self._shown:Fire(self.Instance, self._shownMaid)
+		self._shownMaid:GiveTask(self.Instance:GetPropertyChangedSignal("Parent"):Connect(function()
+			if (not self.Instance.Parent) then
+				self._shownMaid:DoCleaning()
+			end
+		end))
+		self._shownMaid:GiveTask(function()
+			self.Instance = nil
+		end)
+	end
+
+	local function OnChildAdded(child)
 		if (child.Name == childName and not self.Instance) then
 			self.Instance = child
-			self._shown:Fire(child, self._shownMaid)
-			self._shownMaid:GiveTask(child:GetPropertyChangedSignal("Parent"):Connect(function()
-				if (not child.Parent) then
-					self._shownMaid:DoCleaning()
-				end
-			end))
-			self._shownMaid:GiveTask(function()
-				self.Instance = nil
-			end)
+			OnInstanceSet()
 		end
-	end))
+	end
+
+	self._maid:GiveTask(parent.ChildAdded:Connect(OnChildAdded))
+	if (self.Instance) then
+		OnInstanceSet()
+	end
+
 	return self
+
 end
 
 
