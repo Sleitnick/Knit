42c2f64f983145fc7d7c269a5b0d760f42f5fcf4
Bobo <71311544+SilentsReplacement@users.noreply.github.com>
2021-06-14 18:30:28 +0500

    Restructure

--- src/init.lua
+++ src/init.lua
@@ -1,6 +1,7 @@
-if (game:GetService("RunService"):IsServer()) then
-	return require(script.KnitServer)
-else
-	script.KnitServer:Destroy()
-	return require(script.KnitClient)
-end
+local RunService = game:GetService("RunService")
+
+return  RunService:IsServer() and require(script.KnitServer) or  require(script.KnitClient)
+
+
+-- script.KnitServer:Destroy() // Redundant, the client can NEVER see any server scripts, (even if they exist in a container that the client can access them)
+-- since the server never replicates the bytecode of a server script to the client
