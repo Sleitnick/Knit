1bb7f30f123c0fb46a5876bcd8d489ac13d14d5f
Stephen Leitnick <sleitnick@gmail.com>
2021-06-29 01:04:49 -0400

    Cleanup

--- src/Util/Remote/ClientRemoteProperty.lua
+++ src/Util/Remote/ClientRemoteProperty.lua
@@ -28,25 +28,11 @@ function ClientRemoteProperty.new(object)
 	local self = setmetatable({
 		_object = object;
 		_value = nil;
-		-- _isTable = object:IsA("RemoteEvent");
 	}, ClientRemoteProperty)
 
 	local function SetValue(v)
 		self._value = v
 	end
-
-	-- if (self._isTable) then
-	-- 	self.Changed = Signal.new()
-	-- 	self._change = object.OnClientEvent:Connect(function(tbl)
-	-- 		SetValue(tbl)
-	-- 		self.Changed:Fire(tbl)
-	-- 	end)
-	-- 	SetValue(object.TableRequest:InvokeServer())
-	-- else
-	-- 	SetValue(object.Value)
-	-- 	self.Changed = object.Changed
-	-- 	self._change = object.Changed:Connect(SetValue)
-	-- end
 
 	self.Changed = Signal.new()
 
@@ -67,10 +53,6 @@ end
 
 
 function ClientRemoteProperty:Destroy()
-	-- self._change:Disconnect()
-	-- if (self._isTable) then
-	-- 	self.Changed:Destroy()
-	-- end
 	self._change:Disconnect()
 	self.Changed:Destroy()
 end
--- src/Util/Remote/RemoteProperty.lua
+++ src/Util/Remote/RemoteProperty.lua
@@ -4,19 +4,6 @@
 
 --[[
 
-	remoteProperty = RemoteProperty.new(value: any [, overrideClass: string])
-
-	remoteProperty:Get(): any
-	remoteProperty:Set(value: any): void
-	remoteProperty:Replicate(): void   [Only for table values]
-	remoteProperty:GetForPlayer(player: Player): any
-	remoteProperty:SetForPlayer(player: Player, value: any)
-	remoteProperty:ReplicateForPlayer(player: Player, value: any)
-	remoteProperty:Destroy(): void
-
-	remoteProperty.Changed(newValue: any [, player: Player]): Connection
-
-
 	remoteProperty = RemoteProperty.new(defaultValue: any)
 
 	remoteProperty:SetDefault(value: any): void
@@ -29,26 +16,15 @@
 	remoteProperty:ReplicateAll(): any
 	remoteProperty:Destroy()
 
+	remoteProperty.DefaultChanged(newValue: any)
+	remoteProperty.PlayerChanged(player: Player, newValue: any)
+
 --]]
 
 
 local Signal = require(script.Parent.Parent.Signal)
 
 local IS_SERVER = game:GetService("RunService"):IsServer()
-
--- local typeClassMap = {
--- 	boolean = "BoolValue";
--- 	string = "StringValue";
--- 	table = "RemoteEvent";
--- 	CFrame = "CFrameValue";
--- 	Color3 = "Color3Value";
--- 	BrickColor = "BrickColorValue";
--- 	number = "NumberValue";
--- 	Instance = "ObjectValue";
--- 	Ray = "RayValue";
--- 	Vector3 = "Vector3Value";
--- 	["nil"] = "ObjectValue";
--- }
 
 
 local RemoteProperty = {}
@@ -58,79 +34,6 @@ RemoteProperty.__index = RemoteProperty
 function RemoteProperty.Is(object)
 	return (type(object) == "table" and getmetatable(object) == RemoteProperty)
 end
-
-
---[[
-
-
-function RemoteProperty.new(value, overrideClass)
-
-	assert(IS_SERVER, "RemoteProperty can only be created on the server")
-
-	if (overrideClass ~= nil) then
-		assert(type(overrideClass) == "string", "OverrideClass must be a string; got " .. type(overrideClass))
-		assert(overrideClass:match("Value$"), "OverrideClass must be of super type ValueBase (e.g. IntValue); got " .. overrideClass)
-	end
-
-	local t = typeof(value)
-	local class = overrideClass or typeClassMap[t]
-	assert(class, "RemoteProperty does not support type \"" .. t .. "\"")
-
-	local self = setmetatable({
-		_value = value;
-		_type = t;
-		_isTable = (t == "table");
-		_object = Instance.new(class);
-	}, RemoteProperty)
-
-	if (self._isTable) then
-		local req = Instance.new("RemoteFunction")
-		req.Name = "TableRequest"
-		req.Parent = self._object
-		function req.OnServerInvoke(_player)
-			return self._value
-		end
-		self.Changed = Signal.new()
-	else
-		self.Changed = self._object.Changed
-	end
-
-	self:Set(value)
-
-	return self
-
-end
-
-
-function RemoteProperty:Replicate(player)
-	if (self._isTable) then
-		self:Set(self._value, player)
-	end
-end
-
-
-function RemoteProperty:Set(value, player)
-	if (self._isTable) then
-		self._object:FireAllClients(value)
-		self.Changed:Fire(value)
-	else
-		self._object.Value = value
-	end
-	self._value = value
-end
-
-
-function RemoteProperty:Get()
-	return self._value
-end
-
-
-function RemoteProperty:Destroy()
-	self._object:Destroy()
-end
-
-
-]]
 
 
 function RemoteProperty.new(defaultValue)
