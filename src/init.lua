f988689baaa9de435bd4bf4761d6289eb9685c12
aku-e <guidable100@gmail.com>
2021-07-09 13:48:55 +0300

    Add parentheses to conform to Knit coding styles

--- src/init.lua
+++ src/init.lua
@@ -2,7 +2,7 @@ local KnitServer = script:FindFirstChild("KnitServer")
 if (game:GetService("RunService"):IsServer()) then
 	return require(KnitServer)
 else
-	if KnitServer then
+	if (KnitServer) then
 		KnitServer:Destroy()
 	end
 	return require(script.KnitClient)
