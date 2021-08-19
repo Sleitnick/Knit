5238a4ff05475018d48804bbabbb834b0eba2a4d
Stephen Leitnick <sleitnick@gmail.com>
2021-08-19 18:22:47 -0400

    Changelog

--- last_changelog.py
+++ last_changelog.py
@@ -1,8 +1,12 @@
-import re
 import sys
 
-with open("CHANGELOG.md") as changelog_file:
-	changelog = changelog_file.read()
-	index = changelog.find("\n## ")
-	log = changelog[:index].strip()
-	sys.stdout.write(log)
+def get_last_changelog_entry(filename):
+	with open(filename) as changelog_file:
+		changelog = changelog_file.read()
+		index = changelog.find("\n## ")
+		log = changelog[:index].strip()
+		sys.stdout.write(log)
+
+if __name__ == "__main__":
+	filename = sys.argv[1]
+	get_last_changelog_entry(filename)
