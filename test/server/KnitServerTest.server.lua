a178d1e905a7867a38d57d4ebfd5b8a2f01289bb
Stephen Leitnick <sleitnick@gmail.com>
2021-11-04 16:25:46 -0400

    Update tests

--- test/server/KnitServerTest.server.lua
+++ test/server/KnitServerTest.server.lua
@@ -1,5 +1,24 @@
 local Knit = require(game:GetService("ReplicatedStorage").Test.Knit)
 
+local MyService = Knit.CreateService {
+	Name = "MyService";
+	Client = {
+		TestEvent = Knit.CreateSignal();
+	};
+}
+
+function MyService:KnitInit()
+	self.Client.TestEvent:Connect(function(player, msg)
+		print("Got message from client event:", player, msg)
+		self.Client.TestEvent:Fire(player, msg:lower())
+	end)
+end
+
+function MyService.Client:TestMethod(player, msg)
+	print("TestMethod from client:", player, msg)
+	return msg:upper()
+end
+
 Knit.Start():andThen(function()
 	print("KnitServer started")
 end):catch(warn)
