7566b3279ae7b51132665e38d1d098da9db890e7
aku <68713456+aku-e@users.noreply.github.com>
2022-01-27 13:16:39 +0200

    Fix whitespace formatting at line 4

--- src/init.lua
+++ src/init.lua
@@ -1,7 +1,7 @@
 if game:GetService("RunService"):IsServer() then
 	return require(script.KnitServer)
 else
-  local KnitServer = script:FindFirstChild("KnitServer")
+	local KnitServer = script:FindFirstChild("KnitServer")
 	if KnitServer then
 		KnitServer:Destroy()
 	end
