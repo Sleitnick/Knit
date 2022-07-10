cacc3e17142fa2acbaa65e2989ac1c8625781b29
xSwezan <101433236+xSwezan@users.noreply.github.com>
2022-07-10 22:44:35 +0200

    Fixed requiring in Edit mode
    This fixes the problem wich Knit deletes KnitServer upon requiring Knit from Edit mode.

--- src/init.lua
+++ src/init.lua
@@ -1,8 +1,12 @@
 if game:GetService("RunService"):IsServer() then
 	return require(script.KnitServer)
 else
+	local _, notInEdit = pcall(function()
+		game:GetService("RunService"):IsEdit()
+	end)
+	
 	local KnitServer = script:FindFirstChild("KnitServer")
-	if KnitServer then
+	if KnitServer and notInEdit then
 		KnitServer:Destroy()
 	end
 	return require(script.KnitClient)
