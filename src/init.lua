08af311f1b9bf7509a9fa306bbbff3bb3660b4c4
Raild3x <47638612+Raild3x@users.noreply.github.com>
2023-06-26 15:31:30 -0400

    Prevent KnitServer from being destroyed when in Edit
    This commit stems from an issue where if Knit is ever even required inside of a module that is used within a plugin like Hoarcekat, it deletes KnitServer from your game. This small change aims to prevent that

--- src/init.lua
+++ src/init.lua
@@ -1,10 +1,14 @@
 local RunService = game:GetService("RunService")
 
+local IS_EDIT = pcall(function()
+	RunService:IsEdit()
+end)
+
 if RunService:IsServer() then
 	return require(script.KnitServer)
 else
 	local KnitServer = script:FindFirstChild("KnitServer")
-	if KnitServer then
+	if KnitServer and not IS_EDIT then
 		KnitServer:Destroy()
 	end
 	return require(script.KnitClient)
